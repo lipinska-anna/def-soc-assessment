d0ac554f5fd1ba1d0fd3765bdd684134718812e9
Anna Lipinska <a.lipinska@dromedian.com>
2024-12-09 21:47:42 +0100

    update generic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./../../libs/reports/src/lib/reports.routes').then(m => m.REPORTS_ROUTES),
   },
+  {
+    path: "**",
+    loadChildren: () => import('./../../libs/reports/src/lib/reports.routes').then(m => m.REPORTS_ROUTES),
+  }
 ];
