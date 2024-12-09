3d9dc93c237a75c507957c2b3251c23dff425706
Anna Lipinska <a.lipinska@dromedian.com>
2024-12-09 21:33:12 +0100

    add backend endpoint

--- libs/reports/src/lib/report.service.ts
+++ libs/reports/src/lib/report.service.ts
@@ -18,6 +18,6 @@ export class ReportService {
   }
 
   public getReports() {
-    return this.#http.get<SummaryResponse>('/api/report')
+    return this.#http.get<SummaryResponse>('assets/data.json')
   }
 }
